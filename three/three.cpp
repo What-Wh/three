e38fddf6d0159ec2470eb9ac0188735109ad9e20
domaksym69@gmail.com <domaksym69@gmail.com>
2024-09-19 23:26:23 +0300

    Add project files.

---
+++ three/three.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1 task
+    /*char a;
+    cin >> a;
+    if (a >= 65 && a <= 90 || a >= 97 && a <= 122) {
+        cout << "Leter";
+    }
+    else if (a >= 48 && a <= 57) {
+        cout << "Number";
+    }
+    else {
+        cout << "Symbol";
+    }*/
+
+
+    //2 task
+    /*float b;
+    int c;
+    cout << "How much does one minute cost?" << endl;
+    cin >> b;
+    cout << "How many minutes?" << endl;
+    cin >> c;
+    cout << c * b << "grn";*/
+
+
+    //3 task
+    /*int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    const int g = 50;
+    const int h = 20;*/
+    //3.1
+    //cout << a / g * 100 + b * h / g * 100;
+    //3.2
+    //cout << (a - b / 100 * g) / 20;
+    //3.3
+    /*if (a / 100 * g - b * 20 > 0) {
+        cout << a / 100 * g - b * 20;
+    }
+    else {
+        cout << "He won`t get money.";
+    }*/
+
+}
